39259ac8710cc4527cfcbac6ace932a217ed5986
Tushar Kesarwani <tushar.kesarwani@booking.com>
2022-01-26 11:29:09 +0100

    Adding extra exception handling

--- src/main/scala/com/asktushar/playground/utils/SourceHandler.scala
+++ src/main/scala/com/asktushar/playground/utils/SourceHandler.scala
@@ -1,12 +1,29 @@
 package com.asktushar.playground.utils
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.slf4j.{Logger, LoggerFactory}
+
+import java.io.FileNotFoundException
 
 class SourceHandler {
+  private val logger: Logger = LoggerFactory.getLogger(this.getClass)
+
   def readJSONFile(spark: SparkSession, filePath : String) : DataFrame = {
-    spark.read.json(filePath)
+    try {
+      spark.read.json(filePath)
+    } catch {
+      case e: FileNotFoundException =>
+        logger.warn(s"File $filePath Not found: ", e)
+        throw new FileNotFoundException()
+    }
   }
   def readMultilineJSONFile(spark: SparkSession, filePath : String) : DataFrame = {
-    spark.read.option("multiline","true").json(filePath)
+    try {
+      spark.read.option("multiline","true").json(filePath)
+    } catch {
+      case e: FileNotFoundException =>
+        logger.warn(s"File $filePath Not found: ", e)
+        throw new FileNotFoundException()
+    }
   }
 }
--- src/main/scala/com/asktushar/playground/utils/TargetHandler.scala
+++ src/main/scala/com/asktushar/playground/utils/TargetHandler.scala
@@ -1,12 +1,23 @@
 package com.asktushar.playground.utils
 
 import org.apache.spark.sql.{DataFrame, SaveMode}
+import org.slf4j.{Logger, LoggerFactory}
+
+import java.io.FileNotFoundException
 
 class TargetHandler {
+  private val logger: Logger = LoggerFactory.getLogger(this.getClass)
+
   def writeFinalDF(complaintsReport: DataFrame, filePath: String) : Unit = {
-    complaintsReport.write.option("header",value = true)
-      .mode(SaveMode.Append)
-      .option("delimiter", ",")
-      .csv(filePath)
+    try {
+      complaintsReport.write.option("header",value = true)
+        .mode(SaveMode.Append)
+        .option("delimiter", ",")
+        .csv(filePath)
+    } catch {
+      case e : FileNotFoundException =>
+        logger.warn(s"File $filePath Not found: ", e)
+        throw new FileNotFoundException()
+    }
   }
 }
